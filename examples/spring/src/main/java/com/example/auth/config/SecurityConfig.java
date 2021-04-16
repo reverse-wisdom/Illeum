0d386e3a32c9268153aa8f0d8f1a6c5da2f3f1e0
김두상 <ds021346@gmail.com>
2021-04-16 13:55:15 +0900

    Add S04P31D106-26 : spring demo 프로젝트 업로드

---
+++ examples/spring/src/main/java/com/example/auth/config/SecurityConfig.java
@@ -0,0 +1,92 @@
+package com.example.auth.config;
+
+import com.example.auth.jwt.JwtAuthenticationEntryPoint;
+import com.example.auth.jwt.JwtRequestFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
+    @Autowired
+    private UserDetailsService jwtUserDetailsService;
+    @Autowired
+    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception
+    {
+
+        http.
+                httpBasic().disable().
+                cors().and().
+                csrf().disable().
+                sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).
+                
+                and().
+                	authorizeRequests().
+                    antMatchers("/swagger-resources/**").permitAll().//swagger
+                    antMatchers("/swagger-ui.html").permitAll().
+                    antMatchers("/v2/api-docs").permitAll().
+                    antMatchers("/webjars/**").permitAll().
+                and().
+                    authorizeRequests().
+                    antMatchers("/newuser/**").permitAll().
+                and().
+                    authorizeRequests().
+                    antMatchers("/admin/**").hasRole("ADMIN").
+                and().
+                    authorizeRequests().
+                    antMatchers("/user/**").hasAnyRole("USER", "ADMIN").
+                    
+                    
+                and().
+                    authorizeRequests().
+                    anyRequest().
+                    authenticated().
+                and().
+                    exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).
+                and().
+                    addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        // configure AuthenticationManager so that it knows from where to load
+        // user for matching credentials
+        // Use BCryptPasswordEncoder
+        auth.userDetailsService(jwtUserDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+}
