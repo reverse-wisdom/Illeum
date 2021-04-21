9fba3558391b927053495f40ccf74b8c95d2e7f0
구민진 <whxorb44@gmail.com>
2021-04-21 23:41:46 +0900

    Update S04P31D106-13 : front 배포테스트를 위한 테스트용입포트페이지 주석

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,11 +3,11 @@ import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
 
 // 테스트용 임포트 페이지
-import webrtcTest from '../views/test/webrtcTest.vue';
-import webrtcTest2 from '../views/test/webrtcTest2.vue';
-import webrtcTest3 from '../views/test/webrtcTest3.vue';
-import componentTest from '../views/test/componentTest.vue';
-import chartTest from '../views/test/chartTest.vue';
+// import webrtcTest from '../views/test/webrtcTest.vue';
+// import webrtcTest2 from '../views/test/webrtcTest2.vue';
+// import webrtcTest3 from '../views/test/webrtcTest3.vue';
+// import componentTest from '../views/test/componentTest.vue';
+// import chartTest from '../views/test/chartTest.vue';
 
 Vue.use(VueRouter);
 
@@ -19,32 +19,32 @@ const routes = [
   },
 
   // 테스트용 라우터 세팅
-  {
-    path: '/webrtctest',
-    name: 'webrtcTest',
-    component: webrtcTest,
-  },
-  {
-    path: '/webrtctest2',
-    name: 'webrtcTest2',
-    component: webrtcTest2,
-  },
-  {
-    path: '/webrtctest3',
-    name: 'webrtcTest3',
-    component: webrtcTest3,
-  },
-  {
-    path: '/componentTest',
-    name: 'componentTest',
-    component: componentTest,
-  },
-  {
-    path: '/chartTest',
-    name: 'chartTest',
-    component: chartTest,
-  },
-  // end
+  // {
+  //   path: '/webrtctest',
+  //   name: 'webrtcTest',
+  //   component: webrtcTest,
+  // },
+  // {
+  //   path: '/webrtctest2',
+  //   name: 'webrtcTest2',
+  //   component: webrtcTest2,
+  // },
+  // {
+  //   path: '/webrtctest3',
+  //   name: 'webrtcTest3',
+  //   component: webrtcTest3,
+  // },
+  // {
+  //   path: '/componentTest',
+  //   name: 'componentTest',
+  //   component: componentTest,
+  // },
+  // {
+  //   path: '/chartTest',
+  //   name: 'chartTest',
+  //   component: chartTest,
+  // },
+  // // end
   {
     path: '/about',
     name: 'About',
