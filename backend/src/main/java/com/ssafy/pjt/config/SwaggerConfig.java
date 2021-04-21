f103fad15be67b609fc7d3b3184dac2e365b928c
김두상 <ds021346@gmail.com>
2021-04-21 17:47:53 +0900

    Add S04P31D106-26 : base 프로젝트 생성

---
+++ backend/src/main/java/com/ssafy/pjt/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package com.ssafy.pjt.config;
+
+import com.google.common.base.Predicates;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.Parameter;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.boot")))
+				.paths(PathSelectors.ant("/**")).build()
+				.securitySchemes(Arrays.asList(apiKey()));
+	}
+	
+	private ApiKey apiKey() {
+		return new ApiKey("Bearer +accessToken","Authorization","header");
+	}
+}
---
+++ backend/src/main/java/com/ssafy/pjt/service/JwtUserDetailsService.java
@@ -0,0 +1,42 @@
+package com.ssafy.pjt.service;
+
+import com.ssafy.pjt.Repository.MemberRepository;
+import com.ssafy.pjt.dto.Member;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private MemberRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Member member = repository.findByEmail(email);
+
+        List<GrantedAuthority> roles = new ArrayList<>();
+
+        if (member == null) {
+            throw new UsernameNotFoundException("User not found with username: " + email);
+        }
+        if ((member.getRole()).equals("ROLE_ADMIN")) {
+            roles.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+        } else {
+            roles.add(new SimpleGrantedAuthority("ROLE_USER"));
+            roles.add(new SimpleGrantedAuthority("ROLE_HI"));
+        }
+        return new User(member.getEmail(), member.getPassword(), roles);
+    }
+
+}
