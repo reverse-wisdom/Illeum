b4950befdf77118c2cd5d60acd17de3b6d6425a7
현진혁 <wak8835@gmail.com>
2021-04-22 17:05:21 +0900

    Add S04P31D106-39 : 기초 JPA 엔티티 클래스 생성

--- backend/src/main/java/com/ssafy/pjt/dto/Member.java
+++ backend/src/main/java/com/ssafy/pjt/dto/Member.java
@@ -4,29 +4,30 @@ import lombok.Data;
 
 import javax.persistence.*;
 
-import java.util.Collection;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @Entity
 public class Member {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private int uid;
-    
-    @Column(nullable=false, unique=true, length=50)
-    private String email;
-    
-    private String password;
-    
-    @Column(nullable=false, unique=true, length=30)
-    private String name;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int uid;
 
-    private String role;
+	@Column(nullable = false, unique = true, length = 50)
+	private String email;
 
-    private String thumbnail;
-    
-//    @OneToMany(fetch=FetchType.EAGER, cascade= CascadeType.ALL)
-//    @JoinColumn(name="uid")
-//    private Collection<Room> room;
-     
+	private String password;
+
+	@Column(nullable = false, unique = true, length = 30)
+	private String name;
+
+	private String role;
+
+	private String thumbnail;
+
+	@OneToMany(targetEntity = Entrant.class, fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	@JoinColumn(name = "uid")
+	private List<Entrant> entrants = new ArrayList<Entrant>();
+
 }
