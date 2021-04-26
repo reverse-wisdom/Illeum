62b9a00c082aeb6d6cf14d8e59974c90d4fb7e4c
leehyeji <leehyeji11@naver.com>
2021-04-27 03:15:01 +0900

    Create S04P31D106-47: 로그인 보류 DB 컬럼 email 재확인하기,  Navbar UI 변경, 로고 변경, ClassSearch컴포넌트 생성

--- frontend/src/components/layout/Navbar.vue
+++ frontend/src/components/layout/Navbar.vue
@@ -1,21 +1,13 @@
 <template>
   <div id="nav">
     <div class="Navwrapper hover_collapse">
-      <div class="top_navbar">
-        <div class="logo">
-          <img src="@/assets/img/finallogo.png" style="width:100%; height 30%; margin-top:50%; margin-left: 25%" alt="" />
-        </div>
-        <div class="menu">
-          <!-- <div class="hamburger" @click="toggleNav">
-            <span>★</span>
-          </div> -->
-          <div class="profile_wrap">
-            <div class="profile"></div>
-          </div>
-        </div>
+      <!-- sidebar -->
+      <div>
+        <img src="../../assets/img/navbar.jpg" class="Navbg" alt="" />
       </div>
-
       <div class="sidebar" @mouseover="hoverNav">
+        <img src="../../assets/img/greenlogo.png" class="Navlogo" alt="" />
+        <hr />
         <div class="sidebar_inner">
           <ul>
             <li>
@@ -30,7 +22,7 @@
             </li>
             <li>
               <a href="/webrtcTest4">
-                <span class="Navtext">webrtcTest4</span>
+                <span class="Navtext">webrtc Test4</span>
               </a>
             </li>
             <li>
@@ -40,7 +32,7 @@
             </li>
             <li>
               <a href="/chartTest">
-                <span class="Navtext">chartTest</span>
+                <span class="Navtext">Chart Test</span>
               </a>
             </li>
           </ul>
@@ -48,70 +40,12 @@
       </div>
     </div>
   </div>
-
-  <!-- <header>
-      <a class="logo" href="/"><img src="@/assets/img/finallogo.png" alt="logo" /></a>
-      <nav>
-        <ul class="nav__links">
-
-          <router-link to="/">Home</router-link>
-          |
-          <router-link to="/sign">Sign</router-link>
-          |
-          <router-link to="/webrtcTest">webrtcTest</router-link>
-          |
-          <router-link to="/webrtcTest2">webrtcTest2</router-link>
-          |
-          <router-link to="/webrtcTest3">webrtcTest3</router-link>
-          |
-          <router-link to="/webrtcTest4">webrtcTest4</router-link>
-          |
-          <router-link to="/componentTest">componentTest</router-link>
-          |
-          <router-link to="/chartTest">chartTest</router-link>
-        </ul>
-      </nav>
-      <p class="menu cta" @click="menu">Menu</p>
-    </header> -->
 </template>
 <script>
 export default {
   components: {},
   created() {},
-  methods: {
-    // toggleNav() {
-    //   var hamburger = document.querySelector('.hamburger');
-    //   hamburger.addEventListener('click', () => {
-    //     hamburger.closest('.Navwrapper').classList.toggle('click_collapse');
-    //     hamburger.closest('.Navwrapper').classList.toggle('hover_collapse');
-    //   });
-    // },
-    hoverNav() {
-      var li_items = document.querySelectorAll('.sidebar ul li');
-
-      var Navwrapper = document.querySelector('.Navwrapper');
-
-      li_items.forEach((li_item) => {
-        li_item.addEventListener('mouseenter', () => {
-          if (Navwrapper.classList.contains('click_collapse')) {
-            return;
-          } else {
-            li_item.closest('.Navwrapper').classList.remove('hover_collapse');
-          }
-        });
-      });
-
-      li_items.forEach((li_item) => {
-        li_item.addEventListener('mouseleave', () => {
-          if (Navwrapper.classList.contains('click_collapse')) {
-            return;
-          } else {
-            li_item.closest('.Navwrapper').classList.add('hover_collapse');
-          }
-        });
-      });
-    },
-  },
+  methods: {},
 };
 </script>
 
@@ -119,89 +53,70 @@ export default {
 @import url('https://fonts.googleapis.com/css2?family=Baloo+Paaji+2:wght@400;600&display=swap');
 
 * {
-  font-family: 'Baloo Paaji 2', cursive;
+  /* font-family: 'Baloo Paaji 2', cursive; */
   margin: 0;
   padding: 0;
   box-sizing: border-box;
   list-style: none;
   text-decoration: none;
 }
-
-#nav {
-  background: #f5f5f5;
-}
-
-.top_navbar {
-  position: fixed;
-  top: 0;
-  left: 0;
+.main-container {
   width: 100%;
-  height: 60px;
-  background: #fff;
-  /* box-shadow: 1px 0 2px rgba(0, 0, 0, 0.125); */
-  display: flex;
-  align-items: center;
+  height: 100%;
+  background: #000;
 }
-
-.top_navbar .logo {
-  width: 250px;
-  /* border-right: 1px solid #f5f5f5; */
+#nav {
+  padding: 0;
 }
-
-.top_navbar .menu {
-  width: calc(100% - 250px);
-  padding: 0 25px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-
-.top_navbar .hamburger {
-  font-size: 25px;
-  cursor: pointer;
-  color: #005faf;
-}
-
-.top_navbar .hamburger:hover {
-  color: #007dc3;
-}
-
-/* .hover_collapse .sidebar {
-  width: 5%;
-} */
-
-/* .hover_collapse .sidebar ul li a .Navtext {
-  display: none;
-} */
 
 .sidebar {
   position: fixed;
-  top: 20%;
+  top: 0;
   left: 0;
-  width: 20%;
+  width: 22%;
   height: 100%;
-  background: #1cbd8f;
+  background: rgba(0, 0, 0, 0.6);
 }
-
+.Navbg {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 22%;
+  height: 100%;
+  /* background: rgba(0, 0, 0, 0.8) url(../../assets/img/navbar.jpg); */
+}
+hr {
+  border-top: 1px solid rgba(248, 239, 239, 0.5);
+  border-bottom: 1px solid rgba(0, 0, 0);
+  margin-top: 20px;
+}
+.Navlogo {
+  margin-top: 1.5rem;
+  margin-left: 5%;
+  width: 50%;
+  /* height: 10%; */
+}
 .sidebar ul li a {
+  margin-top: 1rem;
   display: block;
-  padding: 30px 20px;
+  padding: 25px 20px;
   /* border-bottom: 1px solid #fff; */
   color: #000;
   transition: all 0.2s ease;
 }
 
-.sidebar ul li a .icon {
+/* .sidebar ul li a .icon {
   font-size: 18px;
   vertical-align: middle;
   transition: background 0.2s ease;
-}
+} */
 
 .sidebar ul li a .Navtext {
-  margin-left: 10px;
   color: #fff;
+  font-size: 15px;
+  font-weight: 100;
   text-transform: uppercase;
-  letter-spacing: 2px;
+  letter-spacing: 3px;
 }
 
 .sidebar ul li a:hover {
@@ -215,10 +130,4 @@ export default {
 .click_collapse .sidebar {
   transition: all 0.2s ease;
 }
-
-/* .click_collapse .main_container {
-  width: calc(100% - 1000px);
-  margin-left: 100px;
-  transition: all 0.2s ease;
-} */
 </style>
