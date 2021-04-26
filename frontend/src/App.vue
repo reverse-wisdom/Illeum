78b3fe9a4916e805fa06a12d9e1c20298d490177
leehyeji <leehyeji11@naver.com>
2021-04-26 14:42:05 +0900

    Create S04P31D106-47: 회원가입/로그인 UI/UX구성, Navbar UI디자인 , webRTC script단 중복부분삭제

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,22 +1,5 @@
 <template>
   <v-app id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
-      |
-      <router-link to="/about">About</router-link>
-      |
-      <router-link to="/webrtcTest">webrtcTest</router-link>
-      |
-      <router-link to="/webrtcTest2">webrtcTest2</router-link>
-      |
-      <router-link to="/webrtcTest3">webrtcTest3</router-link>
-      |
-      <router-link to="/webrtcTest4">webrtcTest4</router-link>
-      |
-      <router-link to="/componentTest">componentTest</router-link>
-      |
-      <router-link to="/chartTest">chartTest</router-link>
-    </div>
     <router-view />
   </v-app>
 </template>
