c5c841e1b24e2054991846ff583186d4c3219f05
leehyeji <pumpkintime84@gmail.com>
2021-04-28 04:18:36 +0900

    Create S04P31D106-66: 프로필 모달창 구현 및 썸네일 이미지 url 바인딩, API 연결은 내일 백단친구들한테  물어보고 구현할 예정

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <h2>about</h2>
+    <v-btn @click="gouserEval">멤버의 평가목록</v-btn>
+    <v-btn @click="gouserClassLi">멤버가 개설한 목록</v-btn>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    gouserEval() {},
+    gouserClassLi() {},
+  },
+};
+</script>
+
+<style></style>
