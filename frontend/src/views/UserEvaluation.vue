3c80e7ef8f9862def232ecd584086d7ecbdfc9a5
leehyeji <pumpkintime84@gmail.com>
2021-04-28 08:50:41 +0900

    Create S04P31D106-66: 일반유저 평가-학습관리 날짜데이터 캘린더 바인딩

---
+++ frontend/src/views/UserEvaluation.vue
@@ -0,0 +1,145 @@
+<template>
+  <div id="user-eval">
+    <div class="user-evaldata">
+      <header>{{ this.$store.state.name }}의 평가</header>
+      <input type="radio" name="slider" checked id="attend" />
+      <input type="radio" name="slider" id="learn" />
+      <input type="radio" name="slider" id="partin" />
+
+      <nav>
+        <label for="attend" class="attend">
+          출결현황
+        </label>
+        <label for="learn" class="learn">
+          학습관리
+        </label>
+        <label for="partin" class="partin">
+          참여도
+        </label>
+
+        <div class="slider"></div>
+      </nav>
+      <section>
+        <div class="content content-1"></div>
+        <div class="content content-2">
+          <UserEval></UserEval>
+        </div>
+        <div class="content content-3"></div>
+      </section>
+    </div>
+  </div>
+</template>
+<script>
+import UserEval from '@/views/evaluation/UserEval';
+export default {
+  components: {
+    UserEval,
+  },
+  data() {
+    return {
+      showcheck2: false,
+    };
+  },
+};
+</script>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+#user-eval {
+  overflow: hidden;
+  margin-top: 0;
+  padding: 0 20px;
+}
+::selection {
+  background: rgba(23, 162, 184, 0.3);
+}
+.user-evaldata {
+  max-width: 700px;
+  width: 100%;
+  margin: 200px auto;
+  padding: 25px 30px 30px 30px;
+  border-radius: 5px;
+  background: #fff;
+  /* box-shadow: 0px 10px 15px rgba(0, 0, 0, 0.1); */
+}
+.user-evaldata header {
+  font-size: 30px;
+  font-weight: 600;
+  padding-bottom: 20px;
+}
+.user-evaldata nav {
+  position: relative;
+  width: 80%;
+  height: 50px;
+  display: flex;
+  align-items: center;
+}
+.user-evaldata nav label {
+  display: block;
+  height: 100%;
+  width: 100%;
+  text-align: center;
+  line-height: 50px;
+  cursor: pointer;
+  position: relative;
+  z-index: 1;
+  color: #000;
+  font-size: 17px;
+  border-radius: 5px;
+  margin: 0 5px;
+  transition: all 0.3s ease;
+}
+.user-evaldata nav label:hover {
+  background: rgba(117, 120, 121, 0.3);
+}
+#attend:checked ~ nav label.attend,
+#learn:checked ~ nav label.learn,
+#partin:checked ~ nav label.partin {
+  color: #fff;
+}
+nav label i {
+  padding-right: 7px;
+}
+nav .slider {
+  position: absolute;
+  height: 100%;
+  width: 20vh;
+  left: 0;
+  bottom: 0;
+  z-index: 0;
+  border-radius: 5px;
+  background: #4b3fed;
+  transition: all 0.3s ease;
+}
+input[type='radio'] {
+  display: none;
+}
+#learn:checked ~ nav .slider {
+  left: 31%;
+}
+#partin:checked ~ nav .slider {
+  left: 67%;
+}
+
+section .content {
+  display: none;
+  background: #fff;
+}
+#attend:checked ~ section .content-1,
+#learn:checked ~ section .content-2,
+#partin:checked ~ section .content-3 {
+  display: block;
+}
+section .content .title {
+  font-size: 21px;
+  font-weight: 500;
+  margin: 30px 0 10px 0;
+}
+section .content p {
+  text-align: justify;
+}
+</style>
--- frontend/src/views/evaluation/UserEval.vue
+++ frontend/src/views/evaluation/UserEval.vue
@@ -1,7 +1,79 @@
-<template></template>
+<template>
+  <div>
+    <span>{{ this.$store.state.uuid }}의 평가목록</span>
+    <v-row justify="center">
+      <!-- <v-date-picker v-model="date" :allowed-dates="allowedDates" class="mt-4" min="mindate" max="maxdate"></v-date-picker> -->
+      <v-date-picker v-model="date" :allowed-dates="allowedDates" class="mt-4" min="1900-04-01" max="2100-10-30"></v-date-picker>
+      <v-col class="d-flex" cols="12" sm="6">
+        <v-menu ref="menu" v-model="menu" :close-on-content-click="false" transition="scale-transition" offset-y min-width="auto">
+          <template v-slot:activator="{ on, attrs }">
+            <v-combobox v-model="date" chips small-chips label="Multiple picker in menu" prepend-icon="mdi-calendar" readonly v-bind="attrs" v-on="on"></v-combobox>
+          </template>
+
+          <v-btn text color="primary" @click="menu = false">
+            Cancel
+          </v-btn>
+          <v-btn text color="primary" @click="$refs.menu.save(dates)">
+            OK
+          </v-btn>
+        </v-menu>
+      </v-col>
+      <!-- <v-date-picker v-model="date"></v-date-picker> -->
+    </v-row>
+  </div>
+</template>
 
 <script>
-export default {};
+import { userEvaluation } from '@/api/auth';
+export default {
+  data() {
+    return {
+      uuid: this.$store.state.uuid,
+      eval: [],
+      items: ['Foo', 'Bar', 'Fizz', 'Buzz'],
+      date: '',
+      menu: false,
+      // date: '2018-03-02',
+      mindate: '2021-04-01',
+      maxdate: '2021-04-30',
+      // arrayDates: ['2021-04-27', '2021-04-23'],
+      arrayDates: [],
+      roomName: [],
+      // eval_date:''
+    };
+  },
+  async created() {
+    // const uuid = this.$store.state.uuid;
+    // console.log(this.uuid);
+    const { data } = await userEvaluation(this.uuid);
+
+    for (var i = 0; i < data.length; i++) {
+      var eval_date = data[i].eval_date.slice(0, 10);
+      this.arrayDates.push(eval_date);
+    }
+    console.log(this.roomName);
+    console.log(this.arrayDates);
+  },
+  methods: {
+    // allowedDates(val) {
+    //   var arrayDates = this.arrayDates;
+    //   arrayDates.indexOf(val) !== -1;
+    // },
+    allowedDates(val) {
+      for (var i = 0; i < this.arrayDates.length; i++) {
+        if (this.arrayDates[i] == val) {
+          // console.log(val);
+          return true;
+        }
+      }
+    },
+    showClass() {
+      console.log('test');
+    },
+
+    // allowedDates: (val) => parseInt(val.split('-')[2], 10) % 2 === 0,
+  },
+};
 </script>
 
 <style></style>
