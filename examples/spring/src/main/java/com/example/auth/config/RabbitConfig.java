19d2bbc0e6820f90d3b58934210bebcc19ca305b
구민진 <whxorb44@gmail.com>
2021-04-29 01:48:04 +0900

    S04P31D106-68 Add : rabbitmq client test

--- examples/spring/src/main/java/com/example/auth/config/RabbitConfig.java
+++ examples/spring/src/main/java/com/example/auth/config/RabbitConfig.java
@@ -1,5 +1,36 @@
 package com.example.auth.config;
 
-public class RabbitConfig {
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+public class RabbitConfig {
+	@Bean
+	Queue workerOneQueue() {
+		return new Queue("workerOne", false);
+	}
+
+	@Bean
+	Queue workerTwoQueue() {
+		return new Queue("workerTwo", false);
+	}
+
+	@Bean
+	TopicExchange topicExchange() {
+		return new TopicExchange("task-exchange");
+	}
+
+	@Bean
+	Binding workerOneBinding(Queue workerOneQueue, TopicExchange topicExchange) {
+		return BindingBuilder.bind(workerOneQueue).to(topicExchange).with("worker.one");
+	}
+
+	@Bean
+	Binding workerTwoBinding(Queue workerTwoQueue, TopicExchange topicExchange) {
+		return BindingBuilder.bind(workerTwoQueue).to(topicExchange).with("worker.two");
+	}
 }
