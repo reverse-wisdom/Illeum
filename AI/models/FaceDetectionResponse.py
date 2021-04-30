87b0f1eaccba45406733c2d1a3773d0438a1ea96
현진혁 <wak8835@gmail.com>
2021-04-30 17:53:23 +0900

    Add S04P31D106-93 : 얼굴 상태에 따른 상태 반환에 따른 서비스 처리 로직 작성

---
+++ AI/models/FaceDetectionResponse.py
@@ -0,0 +1,24 @@
+from OpenVtuber.SimpleFaceModule import SimpleFaceDetectionResult
+
+RESULT_TYPE_ASLEEP = "asleep"
+RESULT_TYPE_ATTENTION = "attention"
+RESULT_TYPE_DISTRACTED = "distracted"
+RESULT_TYPE_AFK = "afk"
+
+
+class FaceDetectionResponse:
+    result: str
+    message: str
+    data: SimpleFaceDetectionResult
+
+    def __init__(self,
+                 result: str,
+                 message: str,
+                 data: SimpleFaceDetectionResult
+                 ):
+        self.result = result
+        self.message = message
+        self.data = data
+
+    def __str__(self) -> str:
+        return self.__dict__.__str__()
