7baaabb53ab4963f99df67131d4bd3ab91bbc933
구민진 <whxorb44@gmail.com>
2021-04-30 16:00:55 +0900

    S04P31D106-68 Add : spring rabbitmqConfig 설정

--- examples/spring/src/main/java/com/example/auth/Controller/RabbitMQController.java
+++ examples/spring/src/main/java/com/example/auth/Controller/RabbitMQController.java
@@ -2,38 +2,51 @@ package com.example.auth.Controller;
 
 import java.util.Map;
 
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.Queue;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.auth.rabbitmq.Sender;
-
-
 @RestController
 @CrossOrigin
 @RequestMapping
 public class RabbitMQController {
-
-	@Autowired
-	Sender sender; 
 	
-	@GetMapping("/rabbitmq/gettest")
-	public String test() {
-		return "test";
-	}
-	@PostMapping("/rabbitmq/messages") 
-	public String sendMessage(final @RequestBody Map<String,String> req ) { 
-		String message = req.get("message"); 
-		try { 
-			sender.send(message); 
-			return "send message success"; 
-		} catch (Exception e ) { 
-			System.out.println(e.getMessage()); 
-			return "send message fail"; 
-			} 
+	@Autowired
+	private AmqpAdmin admin;
+
+	@PostMapping("/rabbitmq/declare")
+	public String declare(final @RequestBody Map<String, String> req) {
+		String queueName = req.get("queue");
+		System.out.println("queue : " + queueName);
+		try {
+			Queue queue = new Queue(queueName, false);
+			admin.declareQueue(queue);
+			//template.convertAndSend(queue, "test declare"); 
+			return "declare success";
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			return "declare fail";
 		}
+	}
+	
+	@PostMapping("/rabbitmq/delete")
+	public String delete(final @RequestBody Map<String, String> req) {
+		String queueName = req.get("queue");
+		System.out.println("queue : " + queueName);
+		try {
+			//Queue queue = new Queue(queueName, false);
+			admin.deleteQueue(queueName);
+			//template.convertAndSend(queue, "test declare"); 
+			return "delete success";
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			return "declare fail";
+		}
+	}
+	
 }
