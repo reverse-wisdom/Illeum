14487ae80669fff3f010668afc8aa11f4df6a209
현진혁 <wak8835@gmail.com>
2021-05-02 03:04:16 +0900

    Update S04P31D106-96 : 일부 테스트 코드 추가 및 router 최종 출력 확인, response repr 수정

--- AI/router/face_router.py
+++ AI/router/face_router.py
@@ -7,6 +7,7 @@ import services.face_service as service
 
 router = APIRouter()
 
+
 # 해당 함수는 swagger 상에서 사진을 넣었을 때에 결과가 잘 나오는지 체크하기 위한 모듈, 실제는 base64로 인코딩된 이미지를 넣을 예정
 @router.post("/test_eval", summary="테스트용 - 현재 태도에 따른 태도 분석")
 async def evaluate_face_background_task_test(uid: int, rid: int, snapshot: UploadFile = File(...)):
@@ -24,3 +25,16 @@ async def evaluate_face_background_task_test(uid: int, rid: int, snapshot: Uploa
 @router.post("/eval", summary="현재 태도에 따른 태도 분석")
 async def evaluate_face_background_task(uid: int, rid: int, snapshot: str):
     return service.face_detection_task(uid, rid, snapshot)
+
+
+if __name__ == "__main__":
+    img = cv2.imread(r"..\some_folder\person.png", cv2.IMREAD_COLOR)
+    jpg_img = cv2.imencode('.jpg', img)
+    b64_string = base64.b64encode(jpg_img[1]).decode('utf-8')
+    import asyncio
+
+    loop = asyncio.get_event_loop()
+    result = loop.run_until_complete(evaluate_face_background_task(2, 2, b64_string))
+    print("Test result.")
+    print(result)
+    pass
