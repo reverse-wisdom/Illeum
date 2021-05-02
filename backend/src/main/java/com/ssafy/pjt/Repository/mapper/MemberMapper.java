afec862bd735e7bfd88a0926ce360d954e3193e3
김두상 <ds021346@gmail.com>
2021-05-02 14:40:24 +0900

    Add S04P31D106-37 : 맴버 출결 확인용 api 추가

--- backend/src/main/java/com/ssafy/pjt/Repository/mapper/MemberMapper.java
+++ backend/src/main/java/com/ssafy/pjt/Repository/mapper/MemberMapper.java
@@ -4,8 +4,10 @@ import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
 
+import com.ssafy.pjt.dto.response.memberAttend;
 public interface MemberMapper {
 	public List<Map<String, Object>> mamberJoinRoom(int uid) throws SQLException;
 	public List<Map<String, Object>> founder(int uid) throws SQLException;
 	public List<Map<String, Object>> memberJoinEvaluation(int uid) throws SQLException;
+	public List<memberAttend> memberAttend(int uid) throws SQLException;
 }
