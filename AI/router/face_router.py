950977a60cfb0e01f8fc35b0a0f7bfc5a8760aed
현진혁 <wak8835@gmail.com>
2021-05-03 17:18:15 +0900

    Fix S04P31D106-99 : fastapi 처리 과정 변경

--- AI/router/face_router.py
+++ AI/router/face_router.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter, File, UploadFile, HTTPException
+from fastapi import APIRouter, File, UploadFile, HTTPException, Form
+from pydantic import BaseModel
+
+import os
 
 import base64
 import cv2
@@ -10,31 +13,30 @@ router = APIRouter()
 
 # 해당 함수는 swagger 상에서 사진을 넣었을 때에 결과가 잘 나오는지 체크하기 위한 모듈, 실제는 base64로 인코딩된 이미지를 넣을 예정
 @router.post("/test_eval", summary="테스트용 - 현재 태도에 따른 태도 분석")
-async def evaluate_face_background_task_test(uid: int, rid: int, snapshot: UploadFile = File(...)):
+async def evaluate_face_background_task_test(uid: int = Form(...), rid: int = Form(...),
+                                             snapshot: UploadFile = File(...)):
     # 이미지를 base64로 컨버팅
-    content = await snapshot.read()
-    nparr = np.frombuffer(content, np.int8)
-    img_np = cv2.imencode(nparr, cv2.IMREAD_COLOR)
-    # 얼굴 인식 모듈 사용
-    # face_service...
-    b64_string = base64.b64encode(img_np[1]).decode('utf-8')
-    return service.face_detection_task(uid, rid, b64_string)
+    content: bytes = await snapshot.read()
+    return service.face_detection_task(uid, rid, content)
+
+
+class Item(BaseModel):
+    uid: int
+    rid: int
+    snapshot: str
 
 
 # 실제 사용
 @router.post("/eval", summary="현재 태도에 따른 태도 분석")
-async def evaluate_face_background_task(uid: int, rid: int, snapshot: str):
-    return service.face_detection_task(uid, rid, snapshot)
+async def evaluate_face_background_task(item: Item):
+    content: bytes = base64.b64decode(item.snapshot).decode('utf-8')
+    return service.face_detection_task(item.uid, item.rid, content)
 
 
 if __name__ == "__main__":
-    img = cv2.imread(r"..\some_folder\person.png", cv2.IMREAD_COLOR)
-    jpg_img = cv2.imencode('.jpg', img)
-    b64_string = base64.b64encode(jpg_img[1]).decode('utf-8')
-    import asyncio
+    img = cv2.imread(r"D:\취직자료백업\프로필사진.jpg", cv2.IMREAD_COLOR)
+    _, img_np = cv2.imencode('.jpg', img)
 
-    loop = asyncio.get_event_loop()
-    result = loop.run_until_complete(evaluate_face_background_task(2, 2, b64_string))
+    result = service.face_detection_task(2, 2, img_np)
     print("Test result.")
     print(result)
-    pass
