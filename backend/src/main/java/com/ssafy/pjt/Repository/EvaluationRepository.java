3ca1155441437277984ae5895be6c8e0cc3ef4af
김두상 <ds021346@gmail.com>
2021-05-03 14:44:10 +0900

    Fix S04P31D106-37 : _붙여라!!!

--- backend/src/main/java/com/ssafy/pjt/Repository/EvaluationRepository.java
+++ backend/src/main/java/com/ssafy/pjt/Repository/EvaluationRepository.java
@@ -5,8 +5,6 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ssafy.pjt.dto.Evaluation;
-import com.ssafy.pjt.dto.Member;
-import com.ssafy.pjt.dto.Room;
 
 public interface EvaluationRepository extends JpaRepository<Evaluation, Integer> {
 	Evaluation findByVid(int vid);
