0b54dc189c98618f464723e6932a05bbb3e54e94
김두상 <ds021346@gmail.com>
2021-05-03 11:31:22 +0900

    S04P31D106-68 Add : webRTC용 컨트롤러 생성

--- backend/src/main/java/com/ssafy/pjt/Repository/EntrantRepository.java
+++ backend/src/main/java/com/ssafy/pjt/Repository/EntrantRepository.java
@@ -12,7 +12,7 @@ public interface EntrantRepository extends JpaRepository<Entrant, Integer> {
 	Entrant findByEid(int rid);
 	Entrant findByUidAndRid(int uid,int rid);
 	List<Entrant> findByUid(int uid);
-	List<Entrant> findByRid(int uid);
+	List<Entrant> findByRid(int rid);
 	Long deleteByEid(int eid);
 	Long deleteByUid(int uid);
 	Long deleteByRid(int rid);
