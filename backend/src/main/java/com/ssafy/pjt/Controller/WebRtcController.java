94e2125e602b51fc8101e854b9a23adc12bac95b
구민진 <whxorb44@gmail.com>
2021-05-03 15:08:50 +0900

    Add S04P31D106-68 : 미팅시작, 미팅입장, 미팅퇴장 알림 추가

--- backend/src/main/java/com/ssafy/pjt/Controller/WebRtcController.java
+++ backend/src/main/java/com/ssafy/pjt/Controller/WebRtcController.java
@@ -1,8 +1,10 @@
 package com.ssafy.pjt.Controller;
 
 import com.ssafy.pjt.Repository.EntrantRepository;
+import com.ssafy.pjt.Repository.MemberRepository;
 import com.ssafy.pjt.Repository.RoomRepository;
 import com.ssafy.pjt.dto.Entrant;
+import com.ssafy.pjt.dto.Member;
 import com.ssafy.pjt.dto.Room;
 
 import io.swagger.annotations.ApiOperation;
@@ -10,6 +12,7 @@ import io.swagger.annotations.ApiOperation;
 import java.util.List;
 
 import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,6 +31,12 @@ public class WebRtcController {
     @Autowired
     private AmqpAdmin admin;
     
+    @Autowired
+    private MemberRepository memberRepository;
+    
+	@Autowired
+	private AmqpTemplate amqpTemplate;
+	
 	@ApiOperation(value = "미팅  입장")
 	@GetMapping(path = "/entrance")
 	public ResponseEntity<?> entrance(@RequestParam(value = "입장자 uid") int uid, @RequestParam(value = "입장하는 방 rid") int rid) {
@@ -35,7 +44,13 @@ public class WebRtcController {
 		try {
 			Room room = roomRepository.findByRid(rid);
 			//
-			
+			Member member = memberRepository.findByUid(uid); // 참여자
+
+            String queueName = "member." + Integer.toString(room.getUid());
+			String message = room.getRoomName() + "에 " + member.getName() + "님이 들어왔습니다.";
+
+			amqpTemplate.convertAndSend(queueName, message);
+
 			//
 			return new ResponseEntity<String>("success", HttpStatus.OK);			
 		} catch (Exception e) {
@@ -50,7 +65,12 @@ public class WebRtcController {
 		try {
 			Room room = roomRepository.findByRid(rid);
 			//
-			
+			Member member = memberRepository.findByUid(uid); // 참여자
+
+            String queueName = "member." + Integer.toString(room.getUid());
+			String message = room.getRoomName() + "에 " + member.getName() + "님이 나갔습니다.";
+
+			amqpTemplate.convertAndSend(queueName, message);
 			//
 			return new ResponseEntity<String>("success", HttpStatus.OK);
 		} catch (Exception e) {
@@ -63,10 +83,12 @@ public class WebRtcController {
 	public ResponseEntity<?> Start(@RequestParam(value = "미팅 시작하는 방 rid") int rid) {
 		
 		try {
-			List<Entrant> entrant = entrantRepository.findByRid(rid);
-			//entrant.get(0).getUid();로 uid마다 보내면 된다라?
+			Room room = roomRepository.findByRid(rid);
+
 			//
-			
+            String roomName = "room." + Integer.toString(rid);
+            String message = room.getRoomName() + "의 강의가 시작되었습니다.";
+			amqpTemplate.convertAndSend(roomName, "", message);
 			//
 			return new ResponseEntity<String>("success", HttpStatus.OK);
 		} catch (Exception e) {
