e6e8b439ccf2ff9a590b1860b08595c7ef875f53
현진혁 <wak8835@gmail.com>
2021-05-04 09:45:11 +0900

    Fix S04P31D106-101 : code smell 개선

--- AI/database/db.py
+++ AI/database/db.py
@@ -4,7 +4,7 @@ from mysql.connector import connect, Error
 
 # 데이터 베이스 정보 및 사용자 정보
 user_name = "illeum"
-user_pwd = "illeum123@"
+user_pwd = "illeum123@" # NOSONAR
 db_host = "k4d106.p.ssafy.io"
 db_name = "illeum"
 
@@ -45,34 +45,3 @@ if __name__ == "__main__":
         print(cursor.fetchone())
     with db.query("SELECT * FROM member WHERE uid = %s", (1,)) as cursor:
         print(cursor.fetchone())
-
-# from sqlalchemy import create_engine
-# from sqlalchemy.ext.declarative import declarative_base
-# from sqlalchemy.orm import sessionmaker, scoped_session
-# from sqlalchemy.pool import NullPool
-
-# # 데이터 베이스 연결을 위한 URL
-# DATABASE = 'mysql+pymysql://%s:%s@%s/%s?charset=utf8' % (
-#     user_name,
-#     user_pwd,
-#     db_host,
-#     db_name
-# )
-# # engine 생성
-# ENGINE = create_engine(
-#     DATABASE,
-#     encoding="utf-8",
-#     echo=False,
-#     poolclass=NullPool
-# )
-# # 싱글턴 패턴으로 DB 연결
-# session = scoped_session(
-#     sessionmaker(
-#         autocommit=False,
-#         autoflush=False,
-#         bind=ENGINE
-#     )
-# )
-#
-# Base = declarative_base()
-# Base.query = session.query_property()
--- AI/main.py
+++ AI/main.py
@@ -5,15 +5,17 @@ from router import face_router
 
 from fastapi.openapi.docs import get_swagger_ui_html, get_redoc_html
 
-app = FastAPI(openapi_url="/api/openapi.json")
+OPEN_API_URL: str = "/api/openapi.json"
+
+app = FastAPI(openapi_url=OPEN_API_URL)
 
 app.include_router(face_router.router, tags=["Face"], prefix="/api/face")
 
 @app.get("/api/docs")
 async def get_swagger_documentation():
-    return get_swagger_ui_html(openapi_url="/api/openapi.json", title="docs")
+    return get_swagger_ui_html(openapi_url=OPEN_API_URL, title="docs")
 
 
 @app.get("/api/redoc")
 async def get_redoc_documentation():
-    return get_redoc_html(openapi_url="/api/openapi.json", title="docs")
+    return get_redoc_html(openapi_url=OPEN_API_URL, title="docs")
