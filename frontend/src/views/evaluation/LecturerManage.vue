7195de58f33966fbd4cb7947cf9f2de97e53145a
leehyeji <pumpkintime84@gmail.com>
2021-05-06 03:45:28 +0900

    Create S04P31D106-55: 로그인한 유저가 개설한 강의가 있을경우, 각 날짜별-클래스별로 청강자(이름,이메일,uid, 학습태도데이터) 정보 불러옴

--- frontend/src/views/evaluation/LecturerManage.vue
+++ frontend/src/views/evaluation/LecturerManage.vue
@@ -1,11 +1,47 @@
 <template>
-  <div>
+  <div class="manage">
     <span>{{ this.$store.state.uuid }}의 학생관리</span>
     <v-row justify="center">
       <v-date-picker v-model="date" @click:date="classNameFetch" :landscape="landscape" locale="ko-kr" :allowed-dates="allowedDates" class="mt-4" min="1900-04-01" max="2100-10-30"></v-date-picker>
       <v-col class="d-flex" cols="12" sm="6">
         <v-select :items="items" :label="date" solo @change="showManage"></v-select>
       </v-col>
+    </v-row>
+    <v-row>
+      <!-- <div v-for="(each, idx) in UsersEval" :key="idx">
+      </div> -->
+      <div class="table-body">
+        <div class="table_responsive">
+          <table>
+            <thead>
+              <tr>
+                <th>No</th>
+                <th>PROFILE</th>
+                <th>NAME</th>
+                <th>E-MAIL</th>
+
+                <th>Evaluation</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              <EachUserManage v-for="(each, idx) in UsersEval" :key="idx" :each="each" :idx="idx"></EachUserManage>
+              <!-- <tr v-for="(each, idx) in UsersEval" :key="idx">
+                <td>{{ idx + 1 }}</td>
+                <td><img src="https://assets.codepen.io/2147066/internal/avatars/users/default.png?fit=crop&format=auto&height=100&version=1608664176&width=100" alt="" /></td>
+                <td>{{ each.name }}</td>
+                <td>{{ each.email }}</td>
+
+                <td>
+                  <span class="action_btn">
+                    <a href="#">자세히보기</a>
+                  </span>
+                </td>
+              </tr> -->
+            </tbody>
+          </table>
+        </div>
+      </div>
     </v-row>
 
     <div style="margin-top: 100px;">
@@ -16,15 +52,17 @@
 </template>
 
 <script>
-import { userEvalList } from '@/api/auth';
 import { fetchRoomname, findByUidClass } from '@/api/class';
+import { fetchEval } from '@/api/evaluation';
 import { partinAll } from '@/api/entrant';
-
+import { getUsers } from '@/api/auth';
+import EachUserManage from '@/views/evaluation/EachUserManage.vue';
 export default {
+  components: {
+    EachUserManage,
+  },
   data() {
     return {
-      uuid: this.$store.state.uuid,
-      token: this.$store.state.token,
       name: '',
       manageClass: [],
       items: [],
@@ -42,21 +80,31 @@ export default {
       firstUser: '',
       fetchRoomlen: 0,
       manageUsers: [],
+      UsersEval: [
+        // {
+        //   userEval: null,
+        //   user: null,
+        // },
+      ],
       manageClasscheck: false,
     };
   },
 
   async created() {
-    const { data } = await findByUidClass(this.token, this.uuid);
+    const token = this.$store.state.token;
+    const uuid = this.$store.state.uuid;
+    const { data } = await findByUidClass(token, uuid);
     console.log(data);
     for (var i = 0; i < data.length; i++) {
       var dates = data[i].startTime.slice(0, 10);
       this.arrayDates.push(dates);
     }
     this.manageClass = data;
+    console.log(this.manageClass);
     this.items = [];
     this.manageClasscheck = false;
   },
+  async mounted() {},
   methods: {
     classNameFetch() {
       this.roomName = [];
@@ -76,9 +124,12 @@ export default {
       }
     },
     async showManage(selected) {
+      this.manageClasscheck = true;
+      this.manageUsers = [];
+      this.UsersEval = [];
       for (var i = 0; i < this.manageClass.length; i++) {
-        if (this.manageClass[i].room_name === selected) {
-          const { data } = await fetchRoomname(this.manageClass[i].room_name);
+        if (this.manageClass[i].roomName === selected) {
+          const { data } = await fetchRoomname(this.manageClass[i].roomName);
           const rid = data[0].rid;
           const res = await partinAll();
           for (var j = 0; j < res.data.length; j++) {
@@ -86,9 +137,128 @@ export default {
               this.manageUsers.push({ uid: res.data[j].uid, eid: res.data[j].eid });
             }
           }
+          console.log(this.manageUsers);
+          const res_2 = await fetchEval();
+          console.log(res_2);
+          for (var k = 0; k < this.manageUsers.length; k++) {
+            for (var m = 0; m < res_2.data.length; m++) {
+              if (res_2.data[m].eid == this.manageUsers[k].eid) {
+                //나중에 entrant 유저 정보도 객체형태로 넣어야함
+                this.UsersEval.push(res_2.data[m]);
+                this.UsersEval[this.UsersEval.length - 1]['uid'] = this.manageUsers[k].uid;
+              }
+            }
+          }
         }
       }
+      console.log(this.UsersEval);
     },
   },
 };
 </script>
+<style scoped>
+.manage {
+  max-width: 700px;
+  width: 100%;
+  margin: auto;
+  padding: 25px 30px 30px 30px;
+  border-radius: 5px;
+  background: #fff;
+}
+/* table */
+
+* {
+  box-sizing: border-box;
+}
+
+.table-body {
+  display: flex;
+  font-family: 'Roboto', sans-serif;
+}
+
+.table_responsive {
+  max-width: 900px;
+
+  background-color: #efefef33;
+  padding: 15px;
+  overflow: auto;
+  margin: auto;
+  border-radius: 4px;
+}
+
+table {
+  width: 100%;
+  font-size: 13px;
+  color: #444;
+  white-space: nowrap;
+  border-collapse: collapse;
+}
+
+table > thead {
+  background-color: #1976d2;
+  color: #fff;
+}
+
+table > thead th {
+  padding: 15px;
+}
+
+table th,
+table td {
+  border: 1px solid #00000017;
+  padding: 10px 15px;
+}
+
+table > tbody > tr > td > img {
+  display: inline-block;
+  width: 60px;
+  height: 60px;
+  object-fit: cover;
+  border-radius: 50%;
+  border: 4px solid #fff;
+  box-shadow: 0 2px 6px #0003;
+}
+
+.action_btn {
+  display: flex;
+  justify-content: center;
+  gap: 10px;
+}
+
+.action_btn > a {
+  text-decoration: none;
+  color: #444;
+  background: #fff;
+  border: 1px solid;
+  display: inline-block;
+  padding: 7px 20px;
+  font-weight: bold;
+  border-radius: 3px;
+  transition: 0.3s ease-in-out;
+}
+
+.action_btn > a:nth-child(1) {
+  border-color: #26a69a;
+}
+
+.action_btn > a:nth-child(2) {
+  border-color: orange;
+}
+
+.action_btn > a:hover {
+  box-shadow: 0 3px 8px #0003;
+}
+
+table > tbody > tr {
+  background-color: #fff;
+  transition: 0.3s ease-in-out;
+}
+
+table > tbody > tr:nth-child(even) {
+  background-color: rgb(238, 238, 238);
+}
+
+table > tbody > tr:hover {
+  filter: drop-shadow(0px 2px 6px #0002);
+}
+</style>
