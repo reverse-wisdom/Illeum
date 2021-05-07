f382e64997653861f53654cddd674ccde181b1f0
leehyeji <leehyeji11@naver.com>
2021-05-08 02:07:17 +0900

    Create S04P31D106-115: MyPage 강의자/청강자모드 클래스(준비-진행-완료-클래스종료) 나누어 구현 세부로직은 아직 미완

---
+++ frontend/src/views/user/LearnerCompleted.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h2>수강자-수업시간종료</h2>
+    <div v-if="learnerDoing == true">
+      <p>수강진행중인 강좌가 있음</p>
+      <div v-for="(item, idx) in classLi" :key="idx">
+        <div v-if="new Date(item.end_time) < Date.now() && item.room_state == '준비'">
+          <p>{{ item.rid }}</p>
+          <p>{{ item.founder }}</p>
+          <p>{{ item.room_name }}</p>
+
+          <p>{{ item.room_type }}</p>
+          <p>{{ item.start_time }}</p>
+          <p>{{ item.end_time }}</p>
+        </div>
+      </div>
+    </div>
+    <div v-else>
+      <p>수강진행중인 강좌가 없음</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { usePartinClass } from '@/api/auth.js';
+export default {
+  data() {
+    return {
+      learnerDoing: false,
+      classLi: [],
+    };
+  },
+  async created() {
+    const { data } = await usePartinClass(this.$store.state.uuid);
+    console.log(data);
+    if (data) {
+      this.learnerDoing = true;
+      this.classLi = data;
+    } else {
+    }
+  },
+};
+</script>
+
+<style></style>
