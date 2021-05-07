7e581c1da4144178ce64635167f0179d63423c33
현진혁 <wak8835@gmail.com>
2021-05-07 09:49:53 +0900

    Update S04P31D106-111: AI code smell 개선

--- AI/utils/rabbitmq_util.py
+++ AI/utils/rabbitmq_util.py
@@ -1,23 +1,23 @@
 import json
 from pika import BlockingConnection, BasicProperties, ConnectionParameters, PlainCredentials
 
-from models.Message import Message
+from models.QueueMessage import QueueMessage
 
 credentials = PlainCredentials(username="illeum", password="illeum123")  # NOSONAR
 props = BasicProperties(content_type='application/json', delivery_mode=2)
 
 
-def send_message(exchange: str, message: Message):
+def send_message(exchange: str, message: QueueMessage):
     global credentials, props
     with BlockingConnection(
             ConnectionParameters(host="k4d106.p.ssafy.io", port=5672, credentials=credentials)) as connection:
         connection.channel().basic_publish(
             exchange=exchange,
             routing_key='',
-            body=message.toJSON().encode('utf-8'),
+            body=message.to_json().encode('utf-8'),
             properties=props
         )
 
 
 if __name__ == "__main__":
-    send_message("room.1", Message("test", "테스트 메세지입니다", None))
+    send_message("room.1", QueueMessage("test", "테스트 메세지입니다", None))
