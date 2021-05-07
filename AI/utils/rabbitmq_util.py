a16b77bc98b6b9a11b74a6f028027e8c58df575e
현진혁 <wak8835@gmail.com>
2021-05-07 18:03:41 +0900

    Update S04P31D106-73 : 상태 이상 현황에 대한 메세지를 방이 아닌, 해당 유저와 방 설립자에게만 전달

--- AI/utils/rabbitmq_util.py
+++ AI/utils/rabbitmq_util.py
@@ -7,7 +7,7 @@ credentials = PlainCredentials(username="illeum", password="illeum123")  # NOSON
 props = BasicProperties(content_type='application/json', delivery_mode=2)
 
 
-def send_message(exchange: str, message: QueueMessage):
+def send_message_fanout(exchange: str, message: QueueMessage):
     global credentials, props
     with BlockingConnection(
             ConnectionParameters(host="k4d106.p.ssafy.io", port=5672, credentials=credentials)) as connection:
@@ -19,5 +19,18 @@ def send_message(exchange: str, message: QueueMessage):
         )
 
 
+def send_message_direct(routing_key: str, message: QueueMessage):
+    global credentials, props
+    with BlockingConnection(
+            ConnectionParameters(host="k4d106.p.ssafy.io", port=5672, credentials=credentials)) as connection:
+        connection.channel().basic_publish(
+            exchange="",
+            routing_key=routing_key,
+            body=message.to_json().encode('utf-8'),
+            properties=props
+        )
+
+
 if __name__ == "__main__":
-    send_message("room.1", QueueMessage("test", "테스트 메세지입니다", None))
+    send_message_fanout("room.1", QueueMessage("test", "테스트 메세지입니다", None))
+    send_message_direct("member.10", QueueMessage("test2", "테스트 메세지입니다", None))
