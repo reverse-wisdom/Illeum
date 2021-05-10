af253845ca0244eb6ae7e9499f98235247d9eada
구민진 <whxorb44@gmail.com>
2021-05-10 09:49:26 +0900

    Update S04P31D106-114 : front-rabbitmq 연결 테스트

--- examples/rabbitmq/listen1.js
+++ examples/rabbitmq/listen1.js
@@ -1,28 +1,31 @@
 #!/usr/bin/env node
 
-var amqp = require('amqplib/callback_api');
-
-amqp.connect('amqp://illeum:illeum123@k4d106.p.ssafy.io:5672', function(error0, connection) {
-    if (error0) {
-        throw error0;
+import mqtt from 'mqtt';
+// import push from 'push.js';
+var wsbroker = 'http://k4d106.p.ssafy.io:5672'; // mqtt websocket enabled broker
+var wsport = 5672; // port for above
+var client = new Paho.MQTT.Client(wsbroker, wsport, '/ws', 'myclientid_' + parseInt(Math.random() * 100, 10));
+// client.onConnectionLost = function (responseObject) {
+//   debug('CONNECTION LOST - ' + responseObject.errorMessage);
+// };
+// client.onMessageArrived = function (message) {
+//   debug('RECEIVE ON ' + message.destinationName + ' PAYLOAD ' + message.payloadString);
+//   print_first(message.payloadString);
+// //   push.create('Test' + msg.content.toString());
+// };
+var options = {
+    timeout: 3,
+    keepAliveInterval: 30,
+    onSuccess: function () {
+        debug("CONNECTION SUCCESS");
+        // client.subscribe('/topic/test', {qos: 1});
+    },
+    onFailure: function (message) {
+        debug("CONNECTION FAILURE - " + message.errorMessage);
     }
-    connection.createChannel(function(error1, channel) {
-        if (error1) {
-            throw error1;
-        }
-
-        var queue = 'member.1';
-
-        channel.assertQueue(queue, {
-            durable: false
-        });
-
-        console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
-
-        channel.consume(queue, function(msg) {
-            console.log(" [x] Received %s", msg.content.toString());
-        }, {
-            noAck: true
-        });
-    });
-});
+};
+if (location.protocol == "https:") {
+    options.useSSL = true;
+}
+debug("CONNECT TO " + wsbroker + ":" + wsport);
+client.connect(options);
--- examples/rabbitmq/listen3.js
+++ examples/rabbitmq/listen3.js
@@ -1,28 +1,34 @@
 #!/usr/bin/env node
 
-var amqp = require('amqplib/callback_api');
+import Stomp from 'webstomp-client'
+import SockJS from 'sockjs-client'
 
-amqp.connect('amqp://illeum:illeum123@k4d106.p.ssafy.io:5672', function(error0, connection) {
-    if (error0) {
-        throw error0;
-    }
-    connection.createChannel(function(error1, channel) {
-        if (error1) {
-            throw error1;
-        }
+const socket  = new SockJS('http://localhost:8080');
+// var ws = 'http://k4d106.p.ssafy.io:5672';
+var client = Stomp.over(socket);
+var headers = {
+    login: 'illeum',
+    passcode: 'illeum123',
+    // additional header
+    // 'client-id': 'my-client-id'
+  };
+client.connect(
+    // headers,
+  (frame) => {
+    // 소켓 연결 성공
+    console.log('소켓 연결 성공', frame);
+    // 서버의 메시지 전송 endpoint를 구독합니다.
+    // 이런형태를 pub sub 구조라고 합니다.
+    client.subscribe('/member.10', (res) => {
+      console.log('구독으로 받은 메시지 입니다.', res.body);
+    //   console.log('구독으로 받은 메시지 입니다.', res.body);
 
-        var queue = 'member.3';
-
-        channel.assertQueue(queue, {
-            durable: false
-        });
-
-        console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
-
-        channel.consume(queue, function(msg) {
-            console.log(" [x] Received %s", msg.content.toString());
-        }, {
-            noAck: true
-        });
+      // 받은 데이터를 json으로 파싱하고 리스트에 넣어줍니다.
+    //   this.recvList.push(JSON.parse(res.body));
     });
-});
+  },
+  (error) => {
+    // 소켓 연결 실패
+    console.log('소켓 연결 실패', error);
+  }
+);
