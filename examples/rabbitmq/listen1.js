56343bcc5dddda4f329b715647a6863f838c272c
구민진 <whxorb44@gmail.com>
2021-05-10 23:13:40 +0900

    Update S04P31D106-114 : App.vue에 rabbitmq 연결(but 큐이름지정불가)

--- examples/rabbitmq/listen1.js
+++ examples/rabbitmq/listen1.js
@@ -1,31 +1,35 @@
 #!/usr/bin/env node
 
-import mqtt from 'mqtt';
-// import push from 'push.js';
-var wsbroker = 'http://k4d106.p.ssafy.io:5672'; // mqtt websocket enabled broker
-var wsport = 5672; // port for above
-var client = new Paho.MQTT.Client(wsbroker, wsport, '/ws', 'myclientid_' + parseInt(Math.random() * 100, 10));
-// client.onConnectionLost = function (responseObject) {
-//   debug('CONNECTION LOST - ' + responseObject.errorMessage);
-// };
-// client.onMessageArrived = function (message) {
-//   debug('RECEIVE ON ' + message.destinationName + ' PAYLOAD ' + message.payloadString);
-//   print_first(message.payloadString);
-// //   push.create('Test' + msg.content.toString());
-// };
-var options = {
-    timeout: 3,
-    keepAliveInterval: 30,
-    onSuccess: function () {
-        debug("CONNECTION SUCCESS");
-        // client.subscribe('/topic/test', {qos: 1});
-    },
-    onFailure: function (message) {
-        debug("CONNECTION FAILURE - " + message.errorMessage);
-    }
-};
-if (location.protocol == "https:") {
-    options.useSSL = true;
-}
-debug("CONNECT TO " + wsbroker + ":" + wsport);
-client.connect(options);
+import WebSocket from 'webstomp-client'
+// import WebSocket from 'ws'
+import SockJS from 'sockjs-client'
+
+var ws = new SockJS('ws://k4d106.p.ssafy.io:15674/ws');
+var client = WebSocket.over(ws);
+var subscription = null;
+var headers = {
+    login: 'illeum',
+    passcode: 'illeum123',
+    // additional header
+    // 'client-id': 'my-client-id'
+  };
+client.connect(
+    headers,
+  (frame) => {
+    // 소켓 연결 성공
+    console.log('소켓 연결 성공', frame);
+    // 서버의 메시지 전송 endpoint를 구독합니다.
+    // 이런형태를 pub sub 구조라고 합니다.
+    client.subscribe('/member.10', (res) => {
+      console.log('구독으로 받은 메시지 입니다.', res.body);
+    //   console.log('구독으로 받은 메시지 입니다.', res.body);
+
+      // 받은 데이터를 json으로 파싱하고 리스트에 넣어줍니다.
+    //   this.recvList.push(JSON.parse(res.body));
+    });
+  },
+  (error) => {
+    // 소켓 연결 실패
+    console.log('소켓 연결 실패', error);
+  }
+);
