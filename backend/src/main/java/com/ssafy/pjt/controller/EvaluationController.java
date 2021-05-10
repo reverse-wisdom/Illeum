8780c66a045b2832e1091186d59dd88edc69a844
김두상 <ds021346@gmail.com>
2021-05-10 20:36:46 +0900

    Add S04P31D106-37 : 평가 조건검색 변경 및 결석 체크 추가

--- backend/src/main/java/com/ssafy/pjt/controller/EvaluationController.java
+++ backend/src/main/java/com/ssafy/pjt/controller/EvaluationController.java
@@ -38,109 +38,144 @@ import io.swagger.annotations.ApiOperation;
 @CrossOrigin
 @RequestMapping("/api/evaluation")
 public class EvaluationController {
-    @Autowired 
-    private EvaluationRepository evaluationRepository;
-    
-    @Autowired
-    private EvaluationMapper evaluationMapper;
-    
-    @Autowired
-    private EntrantRepository entrantRepository;
-    
-    @Autowired
-    private RoomRepository roomRepository;
-    
-    @Autowired
-    RedisTemplate<String, Object> redisTemplate;
-    
-    @ApiOperation(value = "평가 전체조회")
-    @GetMapping(path="/findAll")
-    public Iterable<Evaluation> getAllEvaluation() {
-        return evaluationRepository.findAll();
-    }
-    @ApiOperation(value = "평가 조회")
-    @GetMapping(path="/findByVid")
-    public Evaluation getByVid(@RequestParam int vid) {
-        return evaluationRepository.findByVid(vid);
-    }
-    
-    @ApiOperation(value = "조건으로 평가 조회")
-    @PostMapping(path="/roomEntrantInfo")
-    public ResponseEntity<Object> roomEntrantInfo(@RequestBody insertRoomEvaluationDto dto){
-    	try {
-    		List<findEntrantInfo> evaluation = evaluationMapper.roomEntrantInfo(dto);
-    		if(evaluation.isEmpty()) return new ResponseEntity<>("없음",HttpStatus.NO_CONTENT);
-    		return new ResponseEntity<>(evaluation,HttpStatus.OK);
-    	}
-    	catch (Exception e) {
-    		return new ResponseEntity<>("fail",HttpStatus.BAD_REQUEST);
-		}
-    	
-    }
-    @ApiOperation(value = "평가 생성(출석 체크)")
-    @Transactional
-    @PostMapping(path="/insert")
-    public ResponseEntity<Object> insert(@RequestBody insertEvaluationDto insertDto) {   	
+	@Autowired
+	private EvaluationRepository evaluationRepository;
+
+	@Autowired
+	private EvaluationMapper evaluationMapper;
+
+	@Autowired
+	private EntrantRepository entrantRepository;
+
+	@Autowired
+	private RoomRepository roomRepository;
+
+	@Autowired
+	RedisTemplate<String, Object> redisTemplate;
+
+	@ApiOperation(value = "평가 전체조회")
+	@GetMapping(path = "/findAll")
+	public Iterable<Evaluation> getAllEvaluation() {
+		return evaluationRepository.findAll();
+	}
+
+	@ApiOperation(value = "평가 조회")
+	@GetMapping(path = "/findByVid")
+	public Evaluation getByVid(@RequestParam int vid) {
+		return evaluationRepository.findByVid(vid);
+	}
+
+	@ApiOperation(value = "조건으로 평가 조회")
+	@PostMapping(path = "/roomEntrantInfo")
+	public ResponseEntity<Object> roomEntrantInfo(@RequestBody insertRoomEvaluationDto dto) {
 		try {
-			Entrant entrant = entrantRepository.findByUidAndRid(insertDto.getUid(), insertDto.getRid());	
-			if(entrant == null) return new ResponseEntity<>("방에 참가한 사람이 아닙니다.",HttpStatus.NO_CONTENT);
-			if(evaluationMapper.seachEvaluation(entrant.getEid()) == null) {
+			List<findEntrantInfo> evaluation = evaluationMapper.roomEntrantInfo(dto);
+			if (evaluation.isEmpty())
+				return new ResponseEntity<>("없음", HttpStatus.NO_CONTENT);
+			return new ResponseEntity<>(evaluation, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>("fail", HttpStatus.BAD_REQUEST);
+		}
+
+	}
+
+	@ApiOperation(value = "평가 생성(출석 체크)")
+	@Transactional
+	@PostMapping(path = "/insert")
+	public ResponseEntity<Object> insert(@RequestBody insertEvaluationDto insertDto) {
+		try {
+			Entrant entrant = entrantRepository.findByUidAndRid(insertDto.getUid(), insertDto.getRid());
+			if (entrant == null)
+				return new ResponseEntity<>("방에 참가한 사람이 아닙니다.", HttpStatus.NO_CONTENT);
+			if (evaluationMapper.seachEvaluation(entrant.getEid()) == null) {
 				Room room = roomRepository.findByRid(insertDto.getRid());
-				Evaluation evaluation = new Evaluation();	
-				if(room.getStartTime().plusMinutes(10).isAfter(insertDto.getNow())) {
+				Evaluation evaluation = new Evaluation();
+				if (room.getStartTime().plusMinutes(10).isAfter(insertDto.getNow())) {
 					evaluation.setAttend("정상");
-				}else {
+				} else {
 					evaluation.setAttend("지각");
 				}
 				evaluation.setEid(entrant.getEid());
-				evaluation.setRanking(1000);	
+				evaluation.setRanking(1000);
 				evaluation = evaluationRepository.save(evaluation);
-				return new ResponseEntity<>(evaluation,HttpStatus.OK);
-			}else {
-				return new ResponseEntity<>("이미 출석하였습니다.",HttpStatus.OK);
+				return new ResponseEntity<>(evaluation, HttpStatus.OK);
+			} else {
+				return new ResponseEntity<>("이미 출석하였습니다.", HttpStatus.OK);
 			}
 		} catch (SQLException e1) {
-			return new ResponseEntity<>("fail",HttpStatus.BAD_REQUEST);
-		}   	
-    }
-    
-    @ApiOperation(value = "평가 삭제")
-    @Transactional
-    @DeleteMapping(path="/deleteByVid")
-    public ResponseEntity<Object> deleteByVid(@RequestParam int vid) {
-    	if(evaluationRepository.findByVid(vid) != null) {
-    		try {
-    			evaluationRepository.deleteByVid(vid);
-            }catch (Exception e) {
-            	return new ResponseEntity<>("fail",HttpStatus.BAD_REQUEST);
-    		}   	
-    	}    
-    	return new ResponseEntity<>("success",HttpStatus.OK);
-    }
-    
-    @ApiOperation(value = "평가 수정(vid만 필수)")
-    @Transactional
-    @PutMapping(path="/updateByVid")
-    public ResponseEntity<Object> update(@RequestBody updateEvaluationDto evaluationDto) {
-    	Evaluation evaluation = evaluationRepository.findByVid(evaluationDto.getVid());
-    	
-        if(evaluation == null) return new ResponseEntity<>("평가가 없습니다.",HttpStatus.NO_CONTENT);
-        
-        if(evaluationDto.getEid() != null)evaluation.setEid(evaluationDto.getEid());
-        if(evaluationDto.getEval_date() != null)evaluation.setEvalDate(Date.from(evaluationDto.getEval_date().atZone(ZoneId.systemDefault()).toInstant()));
-        if(evaluationDto.getAttention() != null)evaluation.setAttention(evaluationDto.getAttention());
-        if(evaluationDto.getParticipation() != null)evaluation.setParticipation(evaluationDto.getParticipation());
-        if(evaluationDto.getDistracted() != null)evaluation.setDistracted(evaluationDto.getDistracted());
-        if(evaluationDto.getAsleep() != null)evaluation.setAsleep(evaluationDto.getAsleep());
-        if(evaluationDto.getAfk() != null)evaluation.setAfk(evaluationDto.getAfk());
-        if(evaluationDto.getAttend_time() != null)evaluation.setAttendTime(Date.from(evaluationDto.getAttend_time().atZone(ZoneId.systemDefault()).toInstant()));
-        if(evaluationDto.getRanking() != null)evaluation.setRanking(evaluationDto.getRanking());
-        if(evaluationDto.getAttend() != null)evaluation.setAttend(evaluationDto.getAttend());
-    	try {
-    		evaluationRepository.save(evaluation);
-    	}catch (Exception e) {
-    		return new ResponseEntity<>("fail",HttpStatus.BAD_GATEWAY);
+			return new ResponseEntity<>("fail", HttpStatus.BAD_REQUEST);
 		}
-    	return new ResponseEntity<>("success",HttpStatus.OK);
-    }
+	}
+
+	@ApiOperation(value = "결석 체크")
+	@Transactional
+	@PostMapping(path = "/insertAbsent")
+	public ResponseEntity<Object> inserAbsent(@RequestBody insertEvaluationDto insertDto) {
+		try {
+			Entrant entrant = entrantRepository.findByUidAndRid(insertDto.getUid(), insertDto.getRid());
+			if (entrant == null)
+				return new ResponseEntity<>("방에 참가한 사람이 아닙니다.", HttpStatus.NO_CONTENT);
+			Evaluation evaluation = new Evaluation();
+			evaluation.setAttend("결석");
+			evaluation.setEid(entrant.getEid());
+			evaluation.setRanking(1000);
+			evaluation = evaluationRepository.save(evaluation);
+			return new ResponseEntity<>(evaluation, HttpStatus.OK);
+
+		} catch (Exception e) {
+			return new ResponseEntity<>("fail", HttpStatus.BAD_REQUEST);
+		}
+	}
+
+	@ApiOperation(value = "평가 삭제")
+	@Transactional
+	@DeleteMapping(path = "/deleteByVid")
+	public ResponseEntity<Object> deleteByVid(@RequestParam int vid) {
+		if (evaluationRepository.findByVid(vid) != null) {
+			try {
+				evaluationRepository.deleteByVid(vid);
+			} catch (Exception e) {
+				return new ResponseEntity<>("fail", HttpStatus.BAD_REQUEST);
+			}
+		}
+		return new ResponseEntity<>("success", HttpStatus.OK);
+	}
+
+	@ApiOperation(value = "평가 수정(vid만 필수)")
+	@Transactional
+	@PutMapping(path = "/updateByVid")
+	public ResponseEntity<Object> update(@RequestBody updateEvaluationDto evaluationDto) {
+		Evaluation evaluation = evaluationRepository.findByVid(evaluationDto.getVid());
+
+		if (evaluation == null)
+			return new ResponseEntity<>("평가가 없습니다.", HttpStatus.NO_CONTENT);
+
+		if (evaluationDto.getEid() != null)
+			evaluation.setEid(evaluationDto.getEid());
+		if (evaluationDto.getEval_date() != null)
+			evaluation.setEvalDate(Date.from(evaluationDto.getEval_date().atZone(ZoneId.systemDefault()).toInstant()));
+		if (evaluationDto.getAttention() != null)
+			evaluation.setAttention(evaluationDto.getAttention());
+		if (evaluationDto.getParticipation() != null)
+			evaluation.setParticipation(evaluationDto.getParticipation());
+		if (evaluationDto.getDistracted() != null)
+			evaluation.setDistracted(evaluationDto.getDistracted());
+		if (evaluationDto.getAsleep() != null)
+			evaluation.setAsleep(evaluationDto.getAsleep());
+		if (evaluationDto.getAfk() != null)
+			evaluation.setAfk(evaluationDto.getAfk());
+		if (evaluationDto.getAttend_time() != null)
+			evaluation.setAttendTime(
+					Date.from(evaluationDto.getAttend_time().atZone(ZoneId.systemDefault()).toInstant()));
+		if (evaluationDto.getRanking() != null)
+			evaluation.setRanking(evaluationDto.getRanking());
+		if (evaluationDto.getAttend() != null)
+			evaluation.setAttend(evaluationDto.getAttend());
+		try {
+			evaluationRepository.save(evaluation);
+		} catch (Exception e) {
+			return new ResponseEntity<>("fail", HttpStatus.BAD_GATEWAY);
+		}
+		return new ResponseEntity<>("success", HttpStatus.OK);
+	}
 }
