11fc8bf281d110b9bce5ea1a10a49d3784076bc2
현진혁 <wak8835@gmail.com>
2021-05-13 02:04:29 +0900

    Fix S04P31D106-122 : nginx 상의 잘못된 path 연결 수정 및 CORS 정정

--- frontend/src/api/faceAI.js
+++ frontend/src/api/faceAI.js
@@ -2,16 +2,11 @@ import axios from 'axios';
 
 import { ai } from './index';
 
-function faceAITest(frm) {
-  return ai.post('/test_eval', frm, { headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'multipart/form-data' } });
-}
-
 function faceAI(userData) {
-  // return ai.post('/api/face/eval', frm, { headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'multipart/form-data' } });
   return ai.post('/eval', userData, {
     headers: {
       'content-type': 'application/json',
     },
   });
 }
-export { faceAITest, faceAI };
+export { faceAI };
