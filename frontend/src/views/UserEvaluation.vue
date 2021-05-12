f232dd8907b99771bf89063b67ba5553b357afea
leehyeji <leehyeji11@naver.com>
2021-05-13 07:15:44 +0900

    Fix S04P31D106-55: 청강생모드 출결현황 부분 아날로그/디지털  시계 컴포넌트 삽입

--- frontend/src/views/UserEvaluation.vue
+++ frontend/src/views/UserEvaluation.vue
@@ -1,38 +1,37 @@
 <template>
   <div id="user-eval">
-    <div class="user-evaldata">
-      <v-alert type="info" elevation="3" color="#304FFE" style="margin-top:10%; width:60vw;">
-        <header>{{ this.$store.state.name }}의 평가</header>
-      </v-alert>
-      <input type="radio" name="slider" checked id="attend" />
-      <input type="radio" name="slider" id="learn" />
-      <input type="radio" name="slider" id="partin" />
+    <v-alert type="info" elevation="3" color="#304FFE" style="margin-top:10%; width:60vw;">
+      <header>{{ this.$store.state.name }}의 평가</header>
+    </v-alert>
+    <v-tabs v-model="tab" background-color="transparent" color="basil" slider-size="1">
+      <v-tab>
+        출결현황
+      </v-tab>
+      <v-tab>
+        학습관리
+      </v-tab>
+      <v-tab>
+        참여도
+      </v-tab>
+    </v-tabs>
 
-      <nav>
-        <label for="attend" class="attend">
-          출결현황
-        </label>
-        <label for="learn" class="learn">
-          학습관리
-        </label>
-        <label for="partin" class="partin">
-          참여도
-        </label>
-
-        <div class="slider"></div>
-      </nav>
-      <section>
-        <div class="content content-1">
+    <v-tabs-items v-model="tab">
+      <v-tab-item>
+        <v-card color="basil" flat>
           <UserAttend></UserAttend>
-        </div>
-        <div class="content content-2">
+        </v-card>
+      </v-tab-item>
+      <v-tab-item>
+        <v-card color="basil" flat>
           <UserEval></UserEval>
-        </div>
-        <div class="content content-3">
+        </v-card>
+      </v-tab-item>
+      <v-tab-item>
+        <v-card color="basil" flat>
           <UserPartin></UserPartin>
-        </div>
-      </section>
-    </div>
+        </v-card>
+      </v-tab-item>
+    </v-tabs-items>
   </div>
 </template>
 <script>
@@ -45,106 +44,16 @@ export default {
     UserPartin,
     UserAttend,
   },
+  data() {
+    return {
+      tab: null,
+    };
+  },
 };
 </script>
 
 <style scoped>
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
 #user-eval {
-  overflow: hidden;
-  /* margin-top: 0; */
-  padding: 0 20px;
-}
-::selection {
-  background: rgba(23, 162, 184, 0.3);
-}
-.user-evaldata {
-  max-width: 700px;
-  width: 100%;
   margin: auto;
-  padding: 25px 30px 30px 30px;
-  border-radius: 5px;
-  background: #fff;
-  /* box-shadow: 0px 10px 15px rgba(0, 0, 0, 0.1); */
-}
-.user-evaldata header {
-  font-size: 30px;
-  font-weight: 600;
-  padding-bottom: 20px;
-}
-.user-evaldata nav {
-  position: relative;
-  width: 80%;
-  height: 50px;
-  display: flex;
-  align-items: center;
-}
-.user-evaldata nav label {
-  display: block;
-  height: 100%;
-  width: 100%;
-  text-align: center;
-  line-height: 50px;
-  cursor: pointer;
-  position: relative;
-  z-index: 1;
-  color: #000;
-  font-size: 17px;
-  border-radius: 5px;
-  margin: 0 5px;
-  transition: all 0.3s ease;
-}
-.user-evaldata nav label:hover {
-  background: rgba(117, 120, 121, 0.3);
-}
-#attend:checked ~ nav label.attend,
-#learn:checked ~ nav label.learn,
-#partin:checked ~ nav label.partin {
-  color: #fff;
-}
-nav label i {
-  padding-right: 7px;
-}
-nav .slider {
-  position: absolute;
-  height: 100%;
-  width: 20vh;
-  left: 0;
-  bottom: 0;
-  z-index: 0;
-  border-radius: 5px;
-  background: #304ffe;
-  transition: all 0.3s ease;
-}
-input[type='radio'] {
-  display: none;
-}
-#learn:checked ~ nav .slider {
-  left: 31%;
-}
-#partin:checked ~ nav .slider {
-  left: 67%;
-}
-
-section .content {
-  display: none;
-  background: #fff;
-}
-#attend:checked ~ section .content-1,
-#learn:checked ~ section .content-2,
-#partin:checked ~ section .content-3 {
-  display: block;
-}
-section .content .title {
-  font-size: 21px;
-  font-weight: 500;
-  margin: 30px 0 10px 0;
-}
-section .content p {
-  text-align: justify;
 }
 </style>
