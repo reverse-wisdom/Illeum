a9e3eaecf7ee449fb886e4cfc08f615c804a3698
leehyeji <pumpkintime84@gmail.com>
2021-05-13 07:15:44 +0900

    Fix S04P31D106-55: 청강생모드 출결현황 부분 아날로그/디지털  시계 컴포넌트 삽입

--- frontend/src/views/evaluation/UserAttend.vue
+++ frontend/src/views/evaluation/UserAttend.vue
@@ -1,22 +1,31 @@
 <template>
   <div>
-    <span>{{ this.$store.state.uuid }}의출결현황</span>
-    <v-row justify="center">
-      <v-date-picker
-        v-model="date"
-        width="500"
-        @click:date="classNameFetch"
-        :landscape="landscape"
-        locale="ko-kr"
-        :allowed-dates="allowedDates"
-        class="mt-4"
-        min="1900-04-01"
-        max="2100-10-30"
-      ></v-date-picker>
-      <v-col cols="12" sm="6">
-        <v-select :items="items" :label="date" solo @input="showPartin"></v-select>
+    <v-row>
+      <v-col cols="12" sm="6" md="8">
+        <Clock></Clock>
+        <section class="time-contain">
+          <p class="attend-time time-shadow" v-text="currentTime"></p>
+        </section>
+        <v-col cols="6" md="4">
+          <v-date-picker
+            v-model="date"
+            width="500"
+            @click:date="classNameFetch"
+            :landscape="landscape"
+            locale="ko-kr"
+            :allowed-dates="allowedDates"
+            class="mt-4"
+            min="1900-04-01"
+            max="2100-10-30"
+          ></v-date-picker>
+        </v-col>
       </v-col>
     </v-row>
+    <span>{{ this.$store.state.uuid }}의출결현황</span>
+    <v-col cols="12" sm="5">
+      <v-select :items="items" :label="date" solo @input="showPartin"></v-select>
+    </v-col>
+
     <div style="margin-top: 100px;">
       <h2>show attendence</h2>
       <h2>eval_date:{{ date }}</h2>
@@ -33,10 +42,11 @@
 
 <script>
 import { userEvalList } from '@/api/auth';
-import { fetchRoomname, evaluateList, findByRidClass } from '@/api/class';
-import { fetchConditions } from '@/api/evaluation';
-
+import Clock from '@/views/components/Clock.vue';
 export default {
+  components: {
+    Clock,
+  },
   data() {
     return {
       uuid: this.$store.state.uuid,
@@ -62,6 +72,8 @@ export default {
       fetchRoomlen: 0,
       evalcheck: false,
       selected: true,
+      // digital clock
+      currentTime: null,
     };
   },
 
@@ -79,8 +91,15 @@ export default {
     this.eval = data;
     this.items = [];
     this.evalcheck = false;
+    // digital clock
+    this.currentTime = this.$moment().format('LTS');
+    setInterval(() => this.updateCurrentTime(), 1 * 1000);
   },
   methods: {
+    updateCurrentTime() {
+      this.currentTime = this.$moment().format('LTS');
+    },
+
     classNameFetch() {
       this.items = [];
       for (var i = 0; i < this.eval.length; i++) {
@@ -119,4 +138,30 @@ export default {
 };
 </script>
 
-<style></style>
+<style lang="scss">
+/* digital clock */
+$top-color: LightSteelBlue;
+$bottom-color: LightSalmon;
+
+section.time-contain {
+  // display: flex;
+  flex-direction: row;
+  // align-items: center;
+  padding-top: 140px;
+  background: transparent;
+}
+
+.attend-time {
+  color: #304ffe;
+  font-size: 3em;
+}
+
+h3.is-3:not(:last-child) {
+  margin: 0;
+  padding: 0;
+}
+
+.time-shadow {
+  text-shadow: 0 0 15px rgba(100, 100, 100, 0.35);
+}
+</style>
