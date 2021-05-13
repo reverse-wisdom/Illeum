41ac0143ae63b4544118a78b61d9cf127a908a05
현진혁 <wak8835@gmail.com>
2021-05-13 16:07:28 +0900

    Update S04P31D106-123 : rabbitmq stomp을 nginx 리버스 프록시를 통해 연결하도록 수정

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,39 +6,6 @@
   </v-app>
 </template>
 
-<!--
-<script>
-  class AlertRabbitMQSocket {
-    constructor(uid) {
-      this.uid = uid;
-      let ws = new WebSocket('ws://k4d106.p.ssafy.io:15674/ws');
-      this.client = Stomp.over(ws);
-      this.client.debug = () => {};
-      this.login = 'illeum-guest';
-      this.passcode = 'illeum-guest';
-    }
-
-    connect() {
-      this.client.connect(
-        this.login,
-        this.passcode,
-        () => {
-          this.client.subscribe(`/amq/queue/member.${this.uid}`, (res) => {
-            console.log('구독으로 받은 메시지', res.body);
-          }); //큐명을 지정한경우 시 사용
-        },
-        (error) => {
-          console.log('소켓 연결 실패', error);
-        },
-        '/'
-      );
-    }
-  }
-  var mqSocket = new AlertRabbitMQSocket(10);
-  mqSocket.connect();
-</script>
--->
-
 <style>
 body {
   -ms-overflow-style: none;
