542ccbe7d42086030b291d060cbe0cf4ae9dc50a
윤예준 <dbm01049@gmail.com>
2021-05-16 10:26:24 +0900

    Update S04P31D106-4 LecturerManage.vue, LecSelectedEval.vue 모달 오류 해결, 모달 컴포넌트 랜더링 개선

--- frontend/src/views/evaluation/LecturerManage.vue
+++ frontend/src/views/evaluation/LecturerManage.vue
@@ -80,86 +80,81 @@
                 <td>{{ each.attend }}</td>
                 <td>{{ each.ranking | checkChatRanking }}</td>
                 <td>{{ each.participation }}회 채팅</td>
-
-                <td>
-                  <v-dialog v-model="dialog" persistent max-width="1200px">
-                    <template v-slot:activator="{ on, attrs }">
-                      <v-btn color="primary" dark v-bind="attrs" v-on="on">
-                        DETAIL
-                      </v-btn>
-                    </template>
-                    <v-card>
-                      <v-card-title>
-                        <span class="headline">
-                          <span>{{ each.name }}님의</span>
-                          Evaluation
-                        </span>
-                      </v-card-title>
-                      <v-card-text>
-                        <v-container>
-                          <v-card>
-                            <v-toolbar flat color="primary" dark>
-                              <v-toolbar-title>
-                                <span>{{ each.name }}님의</span>
-                                Evaluation
-                              </v-toolbar-title>
-                            </v-toolbar>
-                            <v-tabs vertical>
-                              <v-tab>
-                                <v-icon left>
-                                  mdi-account
-                                </v-icon>
-                                출결
-                              </v-tab>
-                              <v-tab>
-                                <v-icon left>
-                                  mdi-lock
-                                </v-icon>
-                                학습태도
-                              </v-tab>
-                              <v-tab>
-                                <v-icon left>
-                                  mdi-access-point
-                                </v-icon>
-                                참여현황
-                              </v-tab>
-
-                              <v-tab-item>
-                                <v-card flat>
-                                  <h2>출결</h2>
-                                  <h3>출석시간:{{ each.attend_time }}</h3>
-                                  <h3>지각여부:{{ each.attend }}</h3>
-                                </v-card>
-                              </v-tab-item>
-                              <v-tab-item>
-                                <v-card flat>
-                                  <LecUserEval :each="each" :rid="ridSelected"></LecUserEval>
-                                </v-card>
-                              </v-tab-item>
-                              <v-tab-item>
-                                <v-card flat>
-                                  <LecUserPartin :each="each" :roomData="roomData" :evalUserCnt="userEvalLength" :rid="ridSelected"></LecUserPartin>
-                                </v-card>
-                              </v-tab-item>
-                            </v-tabs>
-                          </v-card>
-                        </v-container>
-                      </v-card-text>
-                      <v-card-actions>
-                        <v-spacer></v-spacer>
-                        <v-btn color="blue darken-1" text @click="dialog = false">
-                          Close
-                        </v-btn>
-                      </v-card-actions>
-                    </v-card>
-                  </v-dialog>
-                </td>
+                <td><v-btn color="primary" @click="openModal(each)">MODAL</v-btn></td>
               </tr>
             </tbody>
           </table>
         </div>
       </div>
     </v-row>
+
+    <!-- modal -->
+    <v-dialog v-model="dialog" persistent max-width="1200px">
+      <v-card>
+        <v-card-title>
+          <span class="headline">
+            <span>{{ modalEach.name }}님의</span>
+            Evaluation
+          </span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-card>
+              <v-toolbar flat color="primary" dark>
+                <v-toolbar-title>
+                  <span>{{ modalEach.name }}님의</span>
+                  Evaluation
+                </v-toolbar-title>
+              </v-toolbar>
+              <v-tabs vertical>
+                <v-tab>
+                  <v-icon left>
+                    mdi-account
+                  </v-icon>
+                  출결
+                </v-tab>
+                <v-tab>
+                  <v-icon left>
+                    mdi-lock
+                  </v-icon>
+                  학습태도
+                </v-tab>
+                <v-tab>
+                  <v-icon left>
+                    mdi-access-point
+                  </v-icon>
+                  참여현황
+                </v-tab>
+
+                <v-tab-item>
+                  <v-card flat>
+                    <h2>출결</h2>
+                    <h3>출석시간:{{ modalEach.attend_time }}</h3>
+                    <h3>지각여부:{{ modalEach.attend }}</h3>
+                  </v-card>
+                </v-tab-item>
+                <v-tab-item :key="modalEach.vid">
+                  <v-card flat>
+                    <LecUserEval :each="modalEach" :rid="ridSelected"></LecUserEval>
+                  </v-card>
+                </v-tab-item>
+                <v-tab-item :key="modalEach.vid">
+                  <v-card flat>
+                    <LecUserPartin :each="modalEach" :roomData="roomData" :evalUserCnt="userEvalLength" :rid="ridSelected"></LecUserPartin>
+                  </v-card>
+                </v-tab-item>
+              </v-tabs>
+            </v-card>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="dialog = false">
+            Close
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
@@ -189,6 +184,7 @@ export default {
       roomNameList: [], // for select box list
       userEval: [], // for table list
       userEvalLength: '', // for table list length
+      modalEach: {}, // for modal value
 
       userEvalAll: [],
 
@@ -278,6 +274,10 @@ export default {
     }
   },
   methods: {
+    openModal(each) {
+      this.modalEach = each;
+      this.dialog = true;
+    },
     allowedDates(val) {
       for (var i = 0; i < this.dateList.length; i++) {
         if (this.dateList[i].date == val) {
